c2e09425923037ad7034664aced6dcbebd08b53d
daniel beja <daniel.beja@post.runi.ac.il>
2023-12-30 00:37:05 +0200

    damka update

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,12 +4,12 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int damkaSize = Integer.parseInt(args[0]);
-		String damkaLine = "";
-		for(int i = 0;i < damkaSize; i++){
-			damkaLine = damkaLine + "* ";
+		String damkaLine = "*";
+		for(int i = 0;i < damkaSize - 1; i++){//create a base to each damka line.
+			damkaLine = damkaLine + " *";
 		}
-		for(int i = 0;i < damkaSize;i++){
-			if(i%2 == 1){
+		for(int i = 0;i < damkaSize;i++){ // print rows.
+			if(i%2 == 1){ //if the row number is uneven print it with space at the begging.
 				System.out.println(" " + damkaLine);
 			}else{
 				System.out.println(damkaLine);
