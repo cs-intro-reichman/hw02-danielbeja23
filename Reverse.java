9345a2a9d66b887ff692789853212c9e647091d3
daniel beja <daniel.beja@post.runi.ac.il>
2023-12-29 22:35:37 +0200

    reverse update

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,16 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String str = args[0];
+		String strRev = "";// The new reversed string.
+		char midChar = 'a';//The middle char.
+		for(int i = 0;i < str.length();i++){
+			strRev = strRev + str.charAt(str.length() - (i+1));//add the chars in reversed to the new string.
+			if(str.length() / 2 == i){//Find the mid char and set it.
+				midChar = strRev.charAt(i);
+			}
+		}	
+		System.out.println(strRev);
+		System.out.println("The middle character is a " + midChar);
 	}
 }
