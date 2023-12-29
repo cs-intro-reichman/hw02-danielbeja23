bdd86c1040cdccc471d4797b20ec8338f7600357
daniel beja <daniel.beja@post.runi.ac.il>
2023-12-30 00:11:50 +0200

    perfect update

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,19 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int ifPerfect = Integer.parseInt(args[0]);
+		String str = ifPerfect + " is a perfect number since " + ifPerfect + " = 1";
+		int sum = 1;
+		for(int i = 2;i <= ifPerfect / 2;i++){
+			if(ifPerfect % i == 0){
+				str =str + " + " + i;
+				sum = sum + i;
+			}
+		}if(sum == ifPerfect){
+			System.out.print(str);
+		}else{
+			System.out.print(ifPerfect + " is not a perfect number");
+		}
+			
 	}
 }
