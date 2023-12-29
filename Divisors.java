39bcad46e619dd60eebf4d0b8d77fcc55cf58e67
daniel beja <daniel.beja@post.runi.ac.il>
2023-12-29 19:31:27 +0200

    First try on Divisors

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,16 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int x = Integer.parseInt(args[0]);
+		if (x == 0){ // If the input is zero, print zero.
+			System.out.println(0);
+		}else{
+			for(int k = 1; (i <= (x / 2)); i++){//Check if x has divisors from 1 to x:2.
+				if(x % k == 0){
+					System.out.println(k);// Print the divisor.
+				}
+			}
+			System.out.println(x); //  Every number is a divisor of himself.
+		}
 	}
 }
