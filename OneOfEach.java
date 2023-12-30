2f076f993ce691ba1b8525f2a4af69c07718e784
daniel beja <daniel.beja@post.runi.ac.il>
2023-12-30 17:34:35 +0200

    update

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,25 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		boolean boy = false;
+		boolean girl = false;
+		double kids =0;
+		String family="";
+		while(!boy || !girl){
+			kids = Math.random() * 2; //0 is a boy and 1 is a girl.
+			if((int)kids == 0){
+				family = family + "b";
+				boy = true;
+			} 
+			if((int)kids == 1){
+				family = family + "g";
+				girl = true;
+			}
+			if(!boy || !girl){
+				family = family + " ";		
+			}
+		}
+		System.out.println(family);
+		System.out.println("You made it... and you now have " + ((family.length() / 2) + 1) + " children.") ;
 	}
 }
